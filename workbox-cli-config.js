d84b538a1863c7561fa849db1eb6c23012a71f20
Kriti Aggarwal <kagga5@sapient.com>
2017-11-10 15:18:02 +0530

    service worker added

---
+++ workbox-cli-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "globDirectory": "dist\\",
+  "globPatterns": [
+    "**/*.{js,txt,ttf,scss,ico,html,log,css}"
+  ],
+  "swDest": "dist/sw-default.js",
+  "globIgnores": [
+    "..\\workbox-cli-config.js"
+  ]
+};
