1736342a0fe40857754a991792969b74a11349a7
Kriti Aggarwal <kagga5@sapient.com>
2017-11-17 13:53:23 +0530

    basic notification added

--- src/main.ts
+++ src/main.ts
@@ -27,6 +27,32 @@ function registerServiceWorker(swName: string) {
       } else {
         console.error('[App] Service Worker API is not supported in current browser');
       }
-};
+      if (!('Notification' in window)) {
+        console.log('This browser does not support notifications!');
+        return;
+      }
+      Notification.requestPermission(function(status) {
+        console.log('Notification permission status:', status);
+        const options = {
+          body: 'New Message from ',
+          icon: 'images/notification-flat.png',
+          vibrate: [100, 50, 100],
+          data: {
+            dateOfArrival: Date.now(),
+            primaryKey: 1
+          },
+          // TODO 2.5 - add actions to the notification
+          // TODO 5.1 - add a tag to the notification
+        };
+        if (Notification['permission'] === 'granted') {
+          navigator.serviceWorker.getRegistration().then(function(reg) {
+            // TODO 2.4 - Add 'options' object to configure the notification
+            reg.showNotification('Hello world!', options);
+          });
+        }
+      });
+}
+
+
 
 
